49a676bf1b76fbc3a365846917ce75c9586bb92b
jppenvose <121736252+jppenvose@users.noreply.github.com>
2023-07-18 16:37:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+
+var x = 'world hello';
+var list = [1,2,33];
+
+console.log(list[0]);
+//1
+console.log(list[1]);
+//2
+console.log(list[33]);
+
+console.log('hello world')
+var thisisadecimal = 3.2;
+var okaycool = thisisadecimal * 2;
+
+console.log(okaycool);
+
+//This demonstrates lists
+var demo_list = [11,11,19,4];
+var fruit_list = ["tomatos", "apple"];
+console.log(demo_list)
+
+var car = {
+	make: 'BMW',
+	year: 2001,
+};
+console.log(car);
+console.log(car['year']);
+console.log(car.year);
+
+var multiply = function (x,y){
+	return x * y;
+}
+multiply(2,3);
+
+var handleReservation = function)_ {
+//verify I cicked the comment:
+	console.log('I just clicked on the button');
+
+	//Get the values of the inputs
+	var firstNameEl = $(".first-name");
+	var numOfPeople = $(".num-of-people");
+
+	console.loc(firstNameEl);
+	console.log(numOfPeopleEl);
+
+	var firstNameElValue = firstNameEl.val();
+	var numOfPeopleValue = numOfPeopleEl.val();
+
+	console.log(firstNameElValue)
+	console.log(numOfPeopleValue)
+}
